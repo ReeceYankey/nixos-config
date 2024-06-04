3e185bbfedea7239904eed11654c932b718a7610
Reece Yankey <reeceyankey@gmail.com>
2024-06-04 18:59:01 -0400

    added home manager

--- flake.nix
+++ flake.nix
@@ -8,20 +8,21 @@
     #};
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"
     nixpkgs.url = "nixpkgs/nixos-23.11";
-    #home-manager.url = "github:nix-community/home-manager/release-23.11"
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       lib = nixpkgs.lib;
       systemSettings = {
-        username = "ryankey"; 
         system = "x86_64-linux"; # system arch
         hostname = "nixos-test"; # hostname
         profile = "personal"; # select a profile defined from my profiles directory
         timezone = "America/New_York"; # select timezone
         locale = "en_US.UTF-8"; # select locale
       };
+      pkgs = nixpkgs.legacyPackages.${systemSettings.system};
       userSettings = {
         username = "ryankey";
         name = "Reece";
@@ -38,16 +39,16 @@
           };
         };
       };
-      #nixosConfigurations = {
-      #  nixos = lib.nixosSystem {
-      #    system = "x86_64-linux";
-      #    modules = [ ./configuration.nix ];
-      #    specialArgs = {
-      #      inherit systemSettings;
-      #      inherit userSettings;
-      #    };
-      #  };
-      #};
+      homeConfigurations = {
+        ${userSettings.username} = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+          extraSpecialArgs = {
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        };
+      };
     };
 
 }
