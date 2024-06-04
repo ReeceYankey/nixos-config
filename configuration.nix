659e3681ec6b121ba55e7956aff3f647670f004b
Reece Yankey <reeceyankey@gmail.com>
2024-06-04 00:03:04 -0400

    added variable substitution for systemSettings and userSettings

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, systemSettings, userSettings, ... }:
 
 {
   imports =
@@ -15,7 +15,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = systemSettings.hostname; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -26,21 +26,21 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = systemSettings.timezone;
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = systemSettings.locale;
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    LC_ADDRESS = systemSettings.locale;
+    LC_IDENTIFICATION = systemSettings.locale;
+    LC_MEASUREMENT = systemSettings.locale;
+    LC_MONETARY = systemSettings.locale;
+    LC_NAME = systemSettings.locale;
+    LC_NUMERIC = systemSettings.locale;
+    LC_PAPER = systemSettings.locale;
+    LC_TELEPHONE = systemSettings.locale;
+    LC_TIME = systemSettings.locale;
   };
 
   # Enable the X11 windowing system.
@@ -80,9 +80,9 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ryankey = {
+  users.users.${userSettings.username} = {
     isNormalUser = true;
-    description = "Reece";
+    description = userSettings.name;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kate
@@ -92,7 +92,7 @@
 
   # Enable automatic login for the user.
   services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "ryankey";
+  services.xserver.displayManager.autoLogin.user = userSettings.username;
 
   # Install firefox.
   programs.firefox.enable = true;
@@ -135,5 +135,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  virtualisation.virtualbox.guest.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
--- flake.nix
+++ flake.nix
@@ -8,18 +8,46 @@
     #};
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    #home-manager.url = "github:nix-community/home-manager/release-23.11"
   };
 
   outputs = { self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
-    in {
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+      systemSettings = {
+        username = "ryankey"; 
+        system = "x86_64-linux"; # system arch
+        hostname = "nixos-test"; # hostname
+        profile = "personal"; # select a profile defined from my profiles directory
+        timezone = "America/New_York"; # select timezone
+        locale = "en_US.UTF-8"; # select locale
       };
+      userSettings = {
+        username = "ryankey";
+        name = "Reece";
+        email = "reeceyankey@gmail.com";
+      };
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          system = systemSettings.system;
+          modules = [ ./configuration.nix ];
+          specialArgs = {
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        };
+      };
+      #nixosConfigurations = {
+      #  nixos = lib.nixosSystem {
+      #    system = "x86_64-linux";
+      #    modules = [ ./configuration.nix ];
+      #    specialArgs = {
+      #      inherit systemSettings;
+      #      inherit userSettings;
+      #    };
+      #  };
+      #};
     };
-  };
 
 }
